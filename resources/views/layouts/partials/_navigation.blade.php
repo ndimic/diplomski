f67c4786b55d80f62eced315e7bc6212995f8979
Nenad Dimic <nenad.dimic34@gmail.com>
2018-06-01 18:28:12 +0200

    New update

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -1,11 +1,11 @@
 <!-- Navigation -->
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-
+	
 	<div class="container">
-
+		
 		<!-- Brand and toggle get grouped for better mobile display -->
 		<div class="navbar-header">
-
+			
 			<button type="button" class="navbar-toggle" data-toggle="collapse"
 			        data-target="#bs-example-navbar-collapse-1">
 				<span class="sr-only">Toggle navigation</span>
@@ -13,27 +13,27 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-
+			
 			<a class="navbar-brand" href="{{ '/' }}">Polovni automobili</a>
-
+		
 		</div>
-
+		
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
+			
 			<ul class="nav navbar-nav">
-
+				
 				<li>
 					<a href="{{ '/cars' }}">Oglasi</a>
 				</li>
 				{{--<li>
 					<a href="{{ '/searchCar' }}">Pretraga</a>
 				</li>--}}
-
+				
 				@if ( Auth::user() )
-
+					
 					@if ( Auth::user()->hasRole( 'admin' ) )
-
+						
 						<li>
 							<a href="{{ '/categories' }}">Admin - Kategorije</a>
 						</li>
@@ -46,7 +46,7 @@
 						<li>
 							<a href="{{ '/addCar' }}">Admin - Postavi oglas</a>
 						</li>
-
+					
 					@else
 						
 						<li>
@@ -55,26 +55,26 @@
 						<li>
 							<a href="{{ '/myAds' }}">Moji oglasi</a>
 						</li>
-						<li>
-							<a href="{{ '/profile' }}">Moj profil</a>
-						</li>
-
+					
 					@endif
-
+					
+					<li>
+						<a href="{{ '/profile' }}">Moj profil</a>
+					</li>
 					<li>
 						<a href="{{ url( '/logout' ) }}">Odjavite se</a>
 					</li>
-
+				
 				@else
-
+					
 					<li>
 						<a href="{{ '/login' }}">Prijavite se</a>
 					</li>
-
+				
 				@endif
-
+			
 			</ul>
-
+		
 		</div>
 		<!-- /.navbar-collapse -->
 	</div>
