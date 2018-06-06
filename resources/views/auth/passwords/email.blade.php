d2d629af689472229f38d62e11ec1996ac41e824
Nenad Dimic <nenad.dimic34@gmail.com>
2018-06-06 14:06:10 +0200

    Changed button color

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -32,7 +32,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-success">
+	                            <button type="submit" class="btn btn" style="background-color: #4a8636">
                                     Pošaljite link za resetovanje lozinke
                                 </button>
                             </div>
