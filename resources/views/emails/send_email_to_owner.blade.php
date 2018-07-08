f10e343d6e122fdc82fda64723bb7df363f5fb78
Nenad Dimic <nenad.dimic34@gmail.com>
2018-07-08 17:03:17 +0200

    Small email changes

--- resources/views/emails/send_email_to_owner.blade.php
+++ resources/views/emails/send_email_to_owner.blade.php
@@ -1,9 +1,7 @@
 <div align="left">
 	
-	Primili ste novu poruku od <i>{{ $sender->name }}</i> vezano za vas oglas broj <b>#{{ $car->id }}
-		- {{ $car->name }}</b>,
-	postavljenog dana <b>{{ $car->created_at }}</b>
-	<br>
+	Primili ste novu poruku od <b><i>{{ $sender->name }} ( ID korisnika: {{ $sender->id }} )</i></b> vezano za vas oglas
+	broj <b>#{{ $car->id }} - {{ $car->name }}</b>, postavljenog dana <b>{{ $car->created_at }}</b><br>
 	
 	<p>
 		<i><q>{{ $request->email_content }}</q></i>
