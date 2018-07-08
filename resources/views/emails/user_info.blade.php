4c5fa756a8219ed4cc5c8a98b17c67e767ef8e97
Nenad Dimic <nenad.dimic34@gmail.com>
2018-07-08 18:09:43 +0200

    More changes

--- resources/views/emails/user_info.blade.php
+++ resources/views/emails/user_info.blade.php
@@ -1,6 +1,6 @@
 <div align="left">
 	
 	Napravljen Vam je nalog na sajtu {{ config( 'app.url' ) }}. Na sajt se mozete ulogovati sa email adresom
-	<b><i>{{ $email }}</i></b>, lozinkom <b><i>{{ $password }}</i></b>. Vaša uloga je "{{ $role }}".
+	<b><i>{{ $email }}</i></b>, lozinkom <b><i>{{ $password }}</i></b>. Vaša uloga je <i>"{{ $role }}"</i>.
 
 </div>
